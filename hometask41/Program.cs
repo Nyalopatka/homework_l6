58e1ab3d0068f28fc301d77e359b3802782f24cb
Nyalopatka <sidnevnikita767@gmail.com>
2023-05-05 14:51:46 +0500

    homework

---
+++ hometask41/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] Array(int size)
+    {
+      int [] array = new int [size];
+      for(int i = 0 ;i < size;i++)
+      {
+          Console.Write("введите число: ");
+          array[i] = Convert.ToInt32(Console.ReadLine());
+      }
+      return array;
+    }
+void ShowArray(int [] array)
+{
+  for(int i = 0;i < array.Length;i++)
+  {
+Console.Write(array[i] + " ");
+  }
+  Console.Write(" -> ");
+}
+void CountArray(int []array)
+{
+  int k = 0;
+  for(int i = 0;i<array.Length;i++)
+  {
+    if(array[i] > 0)
+    {
+     k++;
+    }
+    
+  }
+  Console.WriteLine(k);
+}
+Console.WriteLine("введите кол-во чисел в массиве");
+int size = Convert.ToInt32(Console.ReadLine()); 
+int [] cArray = Array(size);
+ShowArray(cArray);
+CountArray(cArray);
+
---
+++ hometask43/Program.cs
@@ -0,0 +1,20 @@
+﻿void Tochka(double b1,double k1,double b2,double k2)
+{
+ 
+ double cordinateX = (b2 - b1)/(k1-k2);
+ double cordinateY = k1 * cordinateX + b1;
+ Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2},k2 = {k2} ->({cordinateX};{cordinateY})");
+}
+Console.Clear();
+Console.Write("введите b1:");
+double b1  = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Tochka(b1,k1,b2,k2);
+
+
+
